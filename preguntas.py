7a4b8bb30c388fbc6346a17b34fd329da4d33aea
Sebastian Lopez Restrepo <selopezre@unal.edu.co>
2022-05-13 14:57:33 -0500

    Prueba con scor accuracy

--- preguntas.py
+++ preguntas.py
@@ -130,7 +130,7 @@ def pregunta_04():
         estimator=estimator,
         param_grid=param_grid,
         cv = 5,  
-        scoring = "r2"  
+        scoring = "accuracy"  
     )
 
     return gridsearchcv
